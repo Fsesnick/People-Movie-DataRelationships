1b302e3aafe72cc603de04953576bdfaaa486990
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-03-30 16:17:16 -0300

    "many-to-many relationship" challenge

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const { Person, Movie } = models;
 // NOTE: We'll use these variables to assist with the creation
 // of our related data after we've defined the relationships
 // (or associations) between our models.
-// let bradBird;
-// let vinDiesel;
-// let eliMarienthal;
-// let craigTNelson;
-// let hollyHunter;
-// let theIronGiant;
-// let theIncredibles;
+let bradBird;
+let vinDiesel;
+let eliMarienthal;
+let craigTNelson;
+let hollyHunter;
+let theIronGiant;
+let theIncredibles;
 
 console.log('Testing the connection to the database...');
 
@@ -56,11 +56,11 @@ console.log('Testing the connection to the database...');
     console.log(JSON.stringify(peopleInstances, null, 2));
 
     // Update the global variables for the people instances
-    let [ bradBird, vinDiesel, eliMarienthal, craigTNelson, hollyHunter ] = peopleInstances;
-    
+    [ bradBird, vinDiesel, eliMarienthal, craigTNelson, hollyHunter ] = peopleInstances;
+
     // Add Movies to the Database
     console.log('Adding movies to the database...');
-    const movieInstances = await Promise.all([ 
+    const movieInstances = await Promise.all([
       Movie.create({
         title: 'The Iron Giant',
         releaseYear: 1999,
@@ -74,26 +74,85 @@ console.log('Testing the connection to the database...');
     ]);
     console.log(JSON.stringify(movieInstances, null, 2));
 
+    // Update the global variables for the movie instances
+    [theIronGiant, theIncredibles] = movieInstances;
+
+    // NOTE: If the instances for the `actors` association are new
+    // then we can add them at the same time as we add the
+    // movie, but since our actors have already been inserted
+    // into the database, we need to use the following approach.
+
+    // Instance methods added to manage the many-to-many relationship.
+
+    // const p1a = theIronGiant.addActor(vinDiesel);
+    // const p1b = theIronGiant.addActor(eliMarienthal);
+
+    const p1c = theIronGiant.addActors([
+      vinDiesel,
+      eliMarienthal
+    ]);
+
+    // const p1 = theIronGiant.setActors([
+    //   vinDiesel,
+    //   eliMarienthal,
+    // ]);
+
+    const p2 = theIncredibles.setActors([
+      craigTNelson,
+      hollyHunter,
+    ]);
+
+    const actors = await Promise.all([
+      p1c,
+      p2,
+    ]);
+    console.log(JSON.stringify(actors, null, 2));
+
     // Retrieve movies
     const movies = await Movie.findAll({
-      include: [{
-        model: Person,
-        as: 'director',
-      }],
+      // attributes: ['id', 'title', 'releaseYear'],
+      include: [
+        {
+          model: Person,
+          as: 'director',
+        },
+        {
+          model: Person,
+          as: 'actors',
+          // attributes: ['firstName', 'lastName'],
+          through: {
+            // this removes the through model properties from being included
+            attributes: [],
+          },
+        },
+      ],
     });
     console.log(movies.map(movie => movie.get({ plain: true })));
 
     // Retrieve people
     const people = await Person.findAll({
-      include: [{
-        model: Movie,
-        as: 'director',
-      }],
+      // attributes: ['id', 'firstName', 'lastName'],
+      include: [
+        {
+          model: Movie,
+          as: 'director',
+          attributes: ['id', 'title', 'releaseYear'],
+        },
+        {
+          model: Movie,
+          as: 'actor',
+          attributes: ['id', 'title', 'releaseYear'],
+          through: {
+            // this removes the through model properties from being included
+            attributes: [],
+          },
+        },
+      ],
     });
     console.log(JSON.stringify(people, null, 2));
-    
+
     process.exit();
-  } catch (error) {
+  } catch(error) {
     if (error.name === 'SequelizeValidationError') {
       const errors = error.errors.map(err => err.message);
       console.error('Validation errors: ', errors);
